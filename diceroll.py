6e56d6f56df9097ee7999dedbd9ae98f774b0c1b
H-unter <99230734+H-unter@users.noreply.github.com>
2023-11-27 14:34:40 +1000

    Extend plotting functionality for any number of dice

--- diceroll.py
+++ diceroll.py
@@ -7,23 +7,29 @@ import matplotlib.pyplot as plt
 
 def main():
     # prompt = input("Enter Dice: ")  # 2d6+1
-    prompt = "2d6+1"
+    prompt = "4d6+0"
     number_of_dice = int(prompt.split('d')[0])
-    number_of_faces = int(prompt.split('+')[0][-1])
-    modifier = int(prompt.split('+')[1][0])
+    number_of_faces = int(prompt.split('d')[1].split('+')[0])
+    modifier = int(prompt.split('+')[1])
 
     minimum_outcome = number_of_dice + modifier
     maximum_outcome = number_of_dice * number_of_faces + modifier
 
-    print(f"for: {number_of_dice}d{number_of_faces}+{modifier}")
     outcome_to_occurrences = {}
     for i in range(minimum_outcome, maximum_outcome + 1):
         outcome_to_occurrences[i] = 0
 
-    for i in range(0, 10000):
-        result = random.randint(1, number_of_faces) + random.randint(1, number_of_faces) + modifier
-        outcome_to_occurrences[result] += 1
+    simulate_dicerolls(number_of_faces, number_of_dice, modifier, outcome_to_occurrences, 10000)
     plot_values(prompt, outcome_to_occurrences)
+
+
+def simulate_dicerolls(number_of_faces, number_of_dice, modifier, outcome_to_occurrences, trial_number):
+    for i in range(trial_number):
+        result = 0
+        for j in range(number_of_dice):
+            result += (random.randint(1, number_of_faces))
+        result += modifier
+        outcome_to_occurrences[result] += 1
 
 
 def plot_values(prompt, outcome_to_occurrences):
