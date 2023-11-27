fbea004b4ef0163e285bf70aabd9aad74fd46004
H-unter <99230734+H-unter@users.noreply.github.com>
2023-11-27 13:49:13 +1000

    Flesh out probability logic

--- diceroll.py
+++ diceroll.py
@@ -2,9 +2,14 @@
 Hunter Kruger-Ilingworth | Dice roll application
 """
 
-prompt = input("Enter Dice: ") #2d6+1
-dice_number = prompt.split('d')[0]
-dice_type = prompt.split('d')[1][0]
-modifier = prompt.split('d')[1][2]
+prompt = input("Enter Dice: ")  # 2d6+1
+dice_number = int(prompt.split('d')[0])
+dice_type = int(prompt.split('+')[0][-1])
+modifier = int(prompt.split('+')[1][0])
 
 print(f"for: {dice_number}d{dice_type}+{modifier}")
+
+chance = (1 / dice_type) * 100
+for i in range(1, dice_type+1):
+    outcome = i + modifier
+    print(f"Chance of getting {outcome} = {chance:2f}%")
