7339cb2abb3b847835b715cdf2cb96582a9bd361
H-unter <99230734+H-unter@users.noreply.github.com>
2023-11-27 14:04:58 +1000

    Implement simulation of dice roll outcomes

--- diceroll.py
+++ diceroll.py
@@ -1,15 +1,24 @@
 """
 Hunter Kruger-Ilingworth | Dice roll application
 """
+import random
 
-prompt = input("Enter Dice: ")  # 2d6+1
-dice_number = int(prompt.split('d')[0])
-dice_type = int(prompt.split('+')[0][-1])
+# prompt = input("Enter Dice: ")  # 2d6+1
+prompt = "2d6+1"
+number_of_dice = int(prompt.split('d')[0])
+number_of_faces = int(prompt.split('+')[0][-1])
 modifier = int(prompt.split('+')[1][0])
 
-print(f"for: {dice_number}d{dice_type}+{modifier}")
+minimum_outcome = number_of_dice + modifier
+maximum_outcome = number_of_dice * number_of_faces + modifier
 
-chance = (1 / dice_type) * 100
-for i in range(1, dice_type+1):
-    outcome = i + modifier
-    print(f"Chance of getting {outcome} = {chance:2f}%")
+print(f"for: {number_of_dice}d{number_of_faces}+{modifier}")
+outcome_to_occurrences = {}
+for i in range(minimum_outcome, maximum_outcome + 1):
+    outcome_to_occurrences[i] = 0
+
+for i in range(0, 10000):
+    result = random.randint(1, number_of_faces) + random.randint(1, number_of_faces) + modifier
+    outcome_to_occurrences[result] += 1
+
+print(outcome_to_occurrences)
