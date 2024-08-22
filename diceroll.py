962250f3c8b58fc52426ce49aec440ebdb5254c0
Hunter Kruger-Ilingworth <hkrugerilingworth@gmail.com>
2024-08-22 12:19:34 +1000

    Improve diceroll code readability

--- diceroll.py
+++ diceroll.py
@@ -5,17 +5,18 @@ import matplotlib.pyplot
 import math
 import convolution # custom module for convolution of probability distributions
 
-is_results_displayed = False
+is_results_displayed = True
 
 def main():
     """Calculate the probability distribution of a die roll prompt"""
     # prompt = input("Enter Dice: ")  # 2d6+1
-    dice_prompt = "20d10+3"
+    dice_prompt = "10d10+3"
     number_of_dice, number_of_faces, modifier = parse_dice_prompt(dice_prompt)
     outcome_to_probability = calculate_pdf(number_of_faces, number_of_dice, modifier)
+    outcome_to_cumulative_probability = calculate_cdf(outcome_to_probability)
     mean_value = calculate_mean(outcome_to_probability)  # change to "expected_outcome"?
     if is_results_displayed:
-        display_results(dice_prompt, outcome_to_probability)
+        display_numerical_results(dice_prompt, outcome_to_cumulative_probability)
     plot_values(dice_prompt, outcome_to_probability, mean_value)
 
 
@@ -42,6 +43,15 @@ def calculate_pdf(number_of_faces, number_of_dice, modifier):
         pdf = convolution.convolve(pdf, die_probability_distribution)
     return {key: value for key, value in pdf.items() if value != 0}
 
+def calculate_cdf(outcome_to_probability):
+    """Calculate the cumulative distribution function of a dice roll"""
+    cumulative_probability = 0
+    outcome_to_cumulative_probability = {}
+    for outcome, probability in outcome_to_probability.items():
+        cumulative_probability += probability
+        outcome_to_cumulative_probability[outcome] = cumulative_probability
+    return outcome_to_cumulative_probability
+
 
 def calculate_mean(outcome_to_probability):
     """Calculate the mean dice outcome"""
@@ -53,33 +63,33 @@ def calculate_mean(outcome_to_probability):
     mean_value = round(mean_value, 1)
     return mean_value
 
-def display_results(prompt, outcome_to_probability):
-    """Display the results of the dice roll"""
-    for outcome, probability in outcome_to_probability.items():
-        max_outcome = max(outcome_to_probability.keys())
-        cumulative_probability = sum([p for x, p in outcome_to_probability.items() if x <= outcome])
-        cumulative_probability_percentage = cumulative_probability * 100
-        print(f"P(0<x<{outcome}) = {cumulative_probability_percentage:.2f}%;        P({outcome}<x<{max_outcome}) = {100 - cumulative_probability_percentage:.2f}%")
-
+def display_numerical_results(dice_prompt, outcome_to_cumulative_probability):
+    """Display all the numerical insights of the dice roll"""
+    max_outcome = max(outcome_to_cumulative_probability.keys())
+    min_outcome = min(outcome_to_cumulative_probability.keys())
+    
+    for outcome, probability in outcome_to_cumulative_probability.items():
+        cumulative_probability_percentage = probability * 100
+        print(f"P({min_outcome}<=x<={outcome}) = {cumulative_probability_percentage:.3f}%;        P({outcome}<=x<={max_outcome}) = {100 - cumulative_probability_percentage:.3f}%")
 
 def plot_values(prompt, outcome_to_occurrences, mean_value):
     """Plot values given an input dictionary"""
     x_values = list(outcome_to_occurrences.keys())
     y_values = list(outcome_to_occurrences.values())
     x_range = max(x_values) - min(x_values)
-    x_increment = math.ceil(x_range / 25) if x_range >= 25 else 1
-    # print(f"x_increment = {x_increment}")
+    x_tick_increment = math.ceil(x_range / 25) if x_range >= 25 else 1
+    print(f"There are {len(x_values)} bars on this histogram")
 
-    matplotlib.pyplot.bar(x_values, y_values, color='green')
+    matplotlib.pyplot.figure(figsize=(14, 5))
+    matplotlib.pyplot.hist(x_values, bins=len(x_values), weights=y_values, color='#D64650', edgecolor='black')
     matplotlib.pyplot.xlabel('Sum')
     matplotlib.pyplot.ylabel('% Occurrence')
     matplotlib.pyplot.title(f'Histogram of Results from {prompt}')
     matplotlib.pyplot.axvline(x=mean_value, label=f"Mean = {mean_value}", color='r')
     matplotlib.pyplot.legend()
-    matplotlib.pyplot.xticks(range(min(x_values), max(x_values) + 1, x_increment))
+    matplotlib.pyplot.xticks(range(min(x_values), max(x_values) + 1, x_tick_increment))
     matplotlib.pyplot.grid(axis='y', linestyle='--', alpha=0.7)
     matplotlib.pyplot.show()
-
 
 if __name__ == "__main__":
     main()
